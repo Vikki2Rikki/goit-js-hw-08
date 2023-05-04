dabb9e775fb05fd5ec2899fda9fa1f595de51c5a
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-05-04 06:30:20 +0300

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,8 @@ import { galleryItems } from './gallery-items';
 
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 console.log(galleryItems);
